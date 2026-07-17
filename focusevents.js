ceae5118bacb447c9366666aa497faedc28e1f63
n01028447 <n01028447@humber.ca>
2026-07-17 00:58:43 -0400

    Add files via upload

---
+++ focusevents.js
@@ -0,0 +1,9 @@
+function handleFocus() {
+    document.getElementById("idnumber").style.background = 'green';
+    document.getElementById("idnumber").style.color = 'blue';
+}
+
+function handleBlur() {
+    document.getElementById("idnumber").style.background = 'blue';
+        document.getElementById("idnumber").style.color = 'green';
+}
